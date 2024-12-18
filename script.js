29624697af5001aa3202d0865362fd2d78e7335b
Saurabh Singh <saurabhh1703@gmail.com>
2024-12-18 16:09:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-let url="https://api.unsplash.com/photos/random?count=8&client_id=tiPGSSsgw9JBOq3x-dxdjpUKpApceJ7xqjHcua9bGGY&query="
+let url = "https://api.unsplash.com/photos/random?count=8&client_id=tiPGSSsgw9JBOq3x-dxdjpUKpApceJ7xqjHcua9bGGY&query=";
 
-let btn=document.querySelector("button");
-btn.addEventListener("click",async()=>
-{
-    let prompt=document.querySelector("input").value;
-    getImages(prompt);
-});
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
 
-
-async function getImages(prompt)
-{
-    try
-    {
-        let res=await axios.get(url+prompt);
+async function getImages(prompt) {
+    try {
+        let res = await axios.get(url + prompt);
         document.querySelectorAll('.image-gallery img').forEach((img, i) => {
             img.src = res.data[i]?.urls.regular || '';
             img.alt = res.data[i]?.alt_description || 'Image';
-    });
-}
-    catch
-    {
+        });
+    } catch {
         console.log("error");
     }
 }
+
+
+btn.addEventListener("click", () => {
+    let prompt = input.value;
+    getImages(prompt);
+});
+
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        let prompt = input.value;
+        getImages(prompt);
+    }
+});
